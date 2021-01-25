f9ccd14a0c40b22889cce7283ba8e635d4eb2ae1
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2021-01-25 12:31:56 +0300

    ref

--- pages/api/echo/[id].tsx
+++ pages/api/echo/[id].tsx
@@ -4,5 +4,10 @@ export default function getById(req: NextApiRequest, res: NextApiResponse) {
   // res.statusCode = 200
   // res.setHeader('Content-Type', 'application/json')
   // res.end(req.query.id)
-  res.json({yourId: req.query.id})
+
+  res.json({
+    method: JSON.stringify(req.method),
+    req: JSON.stringify(req.body),
+    yourId: req.query.id
+  })
 }
